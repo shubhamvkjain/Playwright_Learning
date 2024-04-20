0af24274f9462ff00ae448592412e9e77690aa0b
Shubham Jain <100926726+shubhamvkjain@users.noreply.github.com>
2024-04-20 14:18:50 +0530

    first commit

---
+++ tests/UIBasics.spec.js
@@ -0,0 +1,20 @@
+const {test} = require("@playwright/test");
+
+test('Browser context test',async ({browser})=>
+{
+
+
+ const context = await browser.newContext();
+ const page = await context.newPage();
+ await page.goto("https://rahulshettyacademy.com/locatorspractice/");
+
+
+});
+
+test('Page Playwright test',async ({page})=>
+{
+
+ await page.goto("https://www.google.com/");
+
+
+});
