2b92d31387189002bfa16b5a99a69f340e90c062
Shubham Jain <100926726+shubhamvkjain@users.noreply.github.com>
2024-04-23 22:08:15 +0530

    Code

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -6,15 +6,20 @@ test('Browser context test',async ({browser})=>
 
  const context = await browser.newContext();
  const page = await context.newPage();
- await page.goto("https://rahulshettyacademy.com/locatorspractice/");
+ await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+ await page.locator("#username").fill("rahulshettyacademy");
+ await page.locator("#password").fill("Learning");
+ await page.locator("#signInBtn").click();
+ console.log(await page.locator("[style*='block']").textContent());
 
-
-});
-
-test.only('Page Playwright test',async ({page})=>
-{
-
- await page.goto("https://www.google.com/");
 
 
 });
+
+//test('Page Playwright test',async ({page})=>
+//{
+
+// await page.goto("https://www.google.com/");
+
+
+//});
